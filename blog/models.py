40ae559e5fc0a645e4d3fb191b333512528e3cb3
Administrator <studentq.work@example.com>
2024-10-01 15:53:23 +0400

    model Article, views Articles

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=20)
+    text = models.CharField(max_length=1000)
+    date = models.DateField(max_length=20)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
+from django.utils import timezone
 from unittest import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 def index(request):
-    return render(request, "blog/index.html")
+    articles = Article.objects.filter(date__lte=timezone.now()).order_by('date')
+
+    return render(request, "blog/index.html", {'articles': articles})
